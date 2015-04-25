cd4a214782eeef0fd3339df57597fa4be61bc13e
David Alexander <dalexander@pacificbiosciences.com>
2015-04-24 17:28:35 -0800

    C# unit tests
    [Tracking bug 24842]
    
    [git-p4: depot-paths = "//depot/software/smrtanalysis/bioinformatics/staging/PostPrimary/pbbam/": change = 149711]

--- cmake/FindCSharp.cmake
+++ cmake/FindCSharp.cmake
@@ -52,7 +52,7 @@ elseif( CSHARP_MONO_FOUND )
   set( CSHARP_VERSION ${CSHARP_MONO_VERSION} CACHE STRING "C# Mono compiler version" FORCE )
   set( CSHARP_COMPILER ${CSHARP_MONO_COMPILER_${CSHARP_MONO_VERSION}} CACHE STRING "Full path to Mono compiler" FORCE )
   set( CSHARP_INTERPRETER ${CSHARP_MONO_INTERPRETER_${CSHARP_MONO_VERSION}} CACHE STRING "Full path to Mono interpretor" FORCE )
-  set( CSHARP_SDK "/sdk:2" CACHE STRING "C# Mono SDK commandline switch (e.g. /sdk:2, /sdk:4, /sdk:5)" )
+  set( CSHARP_SDK "/sdk:4.5" CACHE STRING "C# Mono SDK commandline switch (e.g. /sdk:2, /sdk:4, /sdk:5)" )
 endif( )
 
 # Handle WIN32 specific issues
--- src/swig/WrapCSharp.cmake
+++ src/swig/WrapCSharp.cmake
@@ -3,7 +3,9 @@ find_package(CSharp REQUIRED)
 include (${CSHARP_USE_FILE})
 
 set(PacBioBAM_CSharpLibDir ${PacBioBAM_LibDir}/csharp)
-set(CSharpTestRootDir ${PacBioBAM_TestsDir}/src/csharp)
+set(PacBioBAM_CSharpDLL    ${PacBioBAM_LibDir}/csharp/bin/Debug/PacBio.BAM.dll)
+set(CSharpTestRootDir      ${PacBioBAM_TestsDir}/src/csharp)
+set(NativeLibraryPaths     ${PacBioBAM_CSharpLibDir}:${PacBioBAM_LibDir}:${Htslib_LibDir})
 
 # create wrapper
 file(MAKE_DIRECTORY ${PacBioBAM_CSharpLibDir})
@@ -29,8 +31,9 @@ configure_file(
   ${PacBioBAM_CSharpLibDir}/PacBio.BAM.csproj
 )
 
-
+#
 # "wrapper worked" check
+#
 configure_file(
   ${CSharpTestRootDir}/check_swig.sh.in
   ${CSharpTestRootDir}/check_swig.sh
@@ -44,7 +47,30 @@ add_custom_target(
 )
 add_dependencies(check_swig_csharp ${SWIG_MODULE_PacBioBam_REAL_NAME})
 
+#
+# Unit tests
+#
+if (PacBioBAM_build_tests)
 
+  configure_file(
+    ${CSharpTestRootDir}/TestPbbam.cs.in
+    ${CSharpTestRootDir}/TestPbbam.cs)
 
+  # Not clear how to steer these files where we want them.
+  csharp_add_executable(
+    TestPbbam
+    "${PacBioBAM_CSharpDLL}"
+    "${CSharpTestRootDir}/TestPbbam.cs;")
+  add_dependencies(TestPbbam check_swig_csharp)
 
-# unit tests
+  add_test(
+    NAME CSharpUnitTests
+    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/src/swig
+    COMMAND ${CSHARP_INTERPRETER} TestPbbam.exe)
+
+  set_tests_properties(
+    CSharpUnitTests
+    PROPERTIES
+    ENVIRONMENT "MONO_PATH=${PacBioBAM_CSharpLibDir}/bin/Debug;LD_LIBRARY_PATH=${NativeLibraryPaths};DYLD_LIBRARY_PATH=${NativeLibraryPaths}")
+
+endif() # unit tests
