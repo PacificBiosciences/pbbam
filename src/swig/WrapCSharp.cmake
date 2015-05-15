86077d7dbba0d81b00a61126a2bc7a4631fbc0ee
David Alexander <dalexander@pacificbiosciences.com>
2015-05-15 13:08:41 -0800

    Build the pbbam C# bindings on windows
    [Bug 26314]
    
    [git-p4: depot-paths = "//depot/software/smrtanalysis/bioinformatics/staging/PostPrimary/pbbam/": change = 150807]

--- src/swig/WrapCSharp.cmake
+++ src/swig/WrapCSharp.cmake
@@ -2,12 +2,14 @@
 find_package(CSharp REQUIRED)
 include (${CSHARP_USE_FILE})
 
-set(PacBioBAM_CSharpLibDir ${PacBioBAM_LibDir}/csharp)
-set(PacBioBAM_CSharpDLL    ${PacBioBAM_LibDir}/csharp/bin/Debug/PacBio.BAM.dll)
-set(CSharpTestRootDir      ${PacBioBAM_TestsDir}/src/csharp)
-set(NativeLibraryPaths     ${PacBioBAM_CSharpLibDir}:${PacBioBAM_LibDir}:${Htslib_LibDir})
+set(PacBioBAM_CSharpLibDir  ${PacBioBAM_LibDir}/csharp/PacBio.BAM)
+set(PacBioBAM_CSharpDLL     ${PacBioBAM_CSharpLibDir}/bin/Debug/PacBio.BAM.dll)
+set(CSharpTestRootDir       ${PacBioBAM_TestsDir}/src/csharp)
+set(NativeLibraryPaths      ${PacBioBAM_CSharpLibDir}:${PacBioBAM_LibDir}:${Htslib_LibDir})
 
-# create wrapper
+#
+# Create SWIG wrapper
+#
 file(MAKE_DIRECTORY ${PacBioBAM_CSharpLibDir})
 set(CMAKE_SWIG_OUTDIR ${PacBioBAM_CSharpLibDir})   # ensure any swig files in lib/csharp
 set_source_files_properties(
@@ -23,54 +25,22 @@ set_target_properties(
 )
 add_dependencies(${SWIG_MODULE_PacBioBam_REAL_NAME} pbbam)
 
-# create .csproj for IDEs.  Not strictly necessary to do it this way
-# but prevents the problem of people trying to open the csproj before
-# the .cs files have been generated.
-configure_file(
-  ${PacBioBAM_SwigSourceDir}/PacBio.BAM.csproj.in
-  ${PacBioBAM_CSharpLibDir}/PacBio.BAM.csproj
-)
-
 #
-# "wrapper worked" check
+# Write a csproj, then shell out to build and check the assembly---
+# can't get it working nicely in CMake yet
 #
 configure_file(
-  ${CSharpTestRootDir}/check_swig.sh.in
-  ${CSharpTestRootDir}/check_swig.sh
+   ${PacBioBAM_SwigSourceDir}/PacBio.BAM.csproj.in
+   ${PacBioBAM_CSharpLibDir}/PacBio.BAM.csproj)
+configure_file(
+  ${CSharpTestRootDir}/TestPbbam.cs.in
+  ${CSharpTestRootDir}/TestPbbam.cs)
+configure_file(
+   ${CSharpTestRootDir}/buildAssembly.sh.in
+   buildAssembly.sh)
+add_custom_command(
+  OUTPUT ${PacBioBAM_CSharpDLL}
+  DEPENDS ${SWIG_MODULE_PacBioBam_REAL_NAME}
+  COMMAND ./buildAssembly.sh
 )
-add_custom_target(
-    check_swig_csharp
-    ALL
-    ./check_swig.sh
-    COMMENT "Checking C# wrapper"
-    WORKING_DIRECTORY ${CSharpTestRootDir}
-)
-add_dependencies(check_swig_csharp ${SWIG_MODULE_PacBioBam_REAL_NAME})
-
-#
-# Unit tests
-#
-if (PacBioBAM_build_tests)
-
-  configure_file(
-    ${CSharpTestRootDir}/TestPbbam.cs.in
-    ${CSharpTestRootDir}/TestPbbam.cs)
-
-  # Not clear how to steer these files where we want them.
-  csharp_add_executable(
-    TestPbbam
-    "${PacBioBAM_CSharpDLL}"
-    "${CSharpTestRootDir}/TestPbbam.cs;")
-  add_dependencies(TestPbbam check_swig_csharp)
-
-  add_test(
-    NAME CSharpUnitTests
-    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/src/swig
-    COMMAND ${CSHARP_INTERPRETER} TestPbbam.exe)
-
-  set_tests_properties(
-    CSharpUnitTests
-    PROPERTIES
-    ENVIRONMENT "MONO_PATH=${PacBioBAM_CSharpLibDir}/bin/Debug;LD_LIBRARY_PATH=${NativeLibraryPaths};DYLD_LIBRARY_PATH=${NativeLibraryPaths}")
-
-endif() # unit tests
+add_custom_target(CSharpAssembly ALL DEPENDS ${PacBioBAM_CSharpDLL})
