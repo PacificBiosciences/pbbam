d2395fc45fc3c2779afe02c2084dbc60de2f3974
Armin Toepfer <atoepfer@pacificbiosciences.com>
2015-07-02 13:15:58 -0800

    [Tracking bug 26957] Refactoring build system of bax2bam and pbbam
    [git-p4: depot-paths = "//depot/software/smrtanalysis/bioinformatics/staging/PostPrimary/pbbam/": change = 153783]

--- tests/files.cmake
+++ tests/files.cmake
@@ -39,36 +39,3 @@ set( PacBioBAMTest_CPP
     ${PacBioBAM_TestsDir}/src/test_ZmwQuery.cpp
 
 )
-
-# GoogleTest headers
-set( GTest_H
-
-    ${GTest_IncludeDir}/gtest/gtest-death-test.h
-    ${GTest_IncludeDir}/gtest/gtest-message.h
-    ${GTest_IncludeDir}/gtest/gtest-param-test.h
-    ${GTest_IncludeDir}/gtest/gtest-printers.h
-    ${GTest_IncludeDir}/gtest/gtest-spi.h
-    ${GTest_IncludeDir}/gtest/gtest-test-part.h
-    ${GTest_IncludeDir}/gtest/gtest-typed-test.h
-    ${GTest_IncludeDir}/gtest/gtest.h
-    ${GTest_IncludeDir}/gtest/gtest_pred_impl.h
-    ${GTest_IncludeDir}/gtest/gtest_prod.h
-    ${GTest_IncludeDir}/gtest/internal/gtest-death-test-internal.h
-    ${GTest_IncludeDir}/gtest/internal/gtest-filepath.h
-    ${GTest_IncludeDir}/gtest/internal/gtest-internal.h
-    ${GTest_IncludeDir}/gtest/internal/gtest-linked_ptr.h
-    ${GTest_IncludeDir}/gtest/internal/gtest-param-util-generated.h
-    ${GTest_IncludeDir}/gtest/internal/gtest-param-util.h
-    ${GTest_IncludeDir}/gtest/internal/gtest-port.h
-    ${GTest_IncludeDir}/gtest/internal/gtest-string.h
-    ${GTest_IncludeDir}/gtest/internal/gtest-tuple.h
-    ${GTest_IncludeDir}/gtest/internal/gtest-type-util.h
-
-    ${GTest_SourceDir}/gtest-internal-inl.h
-)
-
-# GoogleTest sources
-set( GTest_CPP
-    ${GTest_SourceDir}/gtest-all.cc
-    ${GTest_SourceDir}/gtest_main.cc
-)
