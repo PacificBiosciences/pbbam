551cc0e86c6bc1af4d0597309b7191f4fdd1cd4a
Brett Bowman <bbowman@pacificbiosciences.com>
2015-09-01 18:29:45 -0800

    [bug 27787]  Making VPR NextRaw() smarter about end-of-iterator cases
    [git-p4: depot-paths = "//depot/software/smrtanalysis/bioinformatics/staging/PostPrimary/pbbam/": change = 158857]

--- src/VirtualPolymeraseReader.cpp
+++ src/VirtualPolymeraseReader.cpp
@@ -88,11 +88,18 @@ std::vector<BamRecord> VirtualPolymeraseReader::NextRaw()
 
     // Current hole number, the smallest of scraps and primary.
     // It can be that the next ZMW is scrap only.
-    int currentHoleNumber = std::min((*primaryIt_).HoleNumber(), (*scrapsIt_).HoleNumber());
+    int currentHoleNumber;
+    if (primaryIt_ == primaryQuery_->end())
+        currentHoleNumber = (*scrapsIt_).HoleNumber();
+    else if (scrapsIt_ == scrapsQuery_->end())
+        currentHoleNumber = (*primaryIt_).HoleNumber();
+    else
+        currentHoleNumber = std::min((*primaryIt_).HoleNumber(), (*scrapsIt_).HoleNumber());
+
     // collect subreads or hqregions
     while (primaryIt_ != primaryQuery_->end() && currentHoleNumber == (*primaryIt_).HoleNumber())
         bamRecordVec.push_back(*primaryIt_++);
-    
+
     // collect scraps
     while (scrapsIt_ != scrapsQuery_->end() && currentHoleNumber == (*scrapsIt_).HoleNumber())
         bamRecordVec.push_back(*scrapsIt_++);
