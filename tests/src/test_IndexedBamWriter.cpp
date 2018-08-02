4d3c09fe30644f2066deafa13ee88152200cd26c
Patrick McNamara <wmcnamara@pacificbiosciences.com>
2018-08-02 09:15:34 -0500

    Revert "Revert "Added GZI-enabled backend for inline PBI generation (pbi-on-the-fly).""
    This reverts commit a38c3af4e4678a981d2d247f356c117c1a13a119.

--- tests/src/test_IndexedBamWriter.cpp
+++ tests/src/test_IndexedBamWriter.cpp
@@ -11,6 +11,8 @@
 #include <pbbam/PbiBuilder.h>
 #include <pbbam/PbiRawData.h>
 
+// clang-format off
+
 TEST(IndexedBamWriter, WritesValidIndex)
 {
     using namespace PacBio::BAM;
@@ -22,40 +24,67 @@ TEST(IndexedBamWriter, WritesValidIndex)
     const BamFile file{inBam};
     const auto& header = file.Header();
 
-    {  // copy file & generate index
+    const std::vector<std::string> expectedQNames{
+        "ArminsFakeMovie/100000/2659_3025",
+        "ArminsFakeMovie/100000/3116_3628",
+        "ArminsFakeMovie/100000/3722_4267",
+        "ArminsFakeMovie/100000/4356_4864",
+        "ArminsFakeMovie/100000/4960_5477",
+        "ArminsFakeMovie/100000/5571_6087",
+        "ArminsFakeMovie/100000/6199_6719",
+        "ArminsFakeMovie/100000/6812_7034",
+        "ArminsFakeMovie/200000/2659_3025",
+        "ArminsFakeMovie/200000/3116_3628",
+        "ArminsFakeMovie/200000/3722_4267",
+        "ArminsFakeMovie/200000/4356_4864",
+        "ArminsFakeMovie/200000/4960_5477",
+        "ArminsFakeMovie/200000/5571_6087",
+        "ArminsFakeMovie/200000/6199_6719",
+        "ArminsFakeMovie/200000/6812_7034",
+        "ArminsFakeMovie/300000/2659_3025",
+        "ArminsFakeMovie/300000/3116_3628",
+        "ArminsFakeMovie/300000/3722_4267",
+        "ArminsFakeMovie/300000/4356_4864",
+        "ArminsFakeMovie/300000/4960_5477",
+        "ArminsFakeMovie/300000/5571_6087",
+        "ArminsFakeMovie/300000/6199_6719",
+        "ArminsFakeMovie/300000/6812_7034"
+    };
+
+    {   // copy file & generate index
 
         BamReader reader{file};
         IndexedBamWriter writer{outBam, header};
-
         BamRecord b;
         while (reader.GetNext(b))
             writer.Write(b);
     }
 
-    // close scope to finalize BAM/PBI output
+    {   // sequential read of new BAM
 
-    {  // check random access using PBI
+        BamReader reader{outBam};
+        BamRecord b;
+        for (size_t i = 0; i < 24; ++i)
+        {
+            reader.GetNext(b);
+            EXPECT_EQ(expectedQNames.at(i), b.FullName());
+        }
+    }
+
+    {   // check random access in new BAM, using companion PBI
 
         const PbiRawData idx{outPbi};
         const auto& offsets = idx.BasicData().fileOffset_;
 
         BamReader reader{outBam};
         BamRecord b;
-        for (size_t i = 0; i < offsets.size(); ++i) {
-            auto canRead = [](BamReader& myReader, BamRecord& record,
-                              size_t loopI) -> ::testing::AssertionResult {
-                if (myReader.GetNext(record))
-                    return ::testing::AssertionSuccess() << "i: " << loopI;
-                else
-                    return ::testing::AssertionFailure() << "i: " << loopI;
-            };
-
+        for (int i = 23; i >=0; --i)
+        {
             reader.VirtualSeek(offsets.at(i));
-            EXPECT_TRUE(canRead(reader, b, i));
+            reader.GetNext(b);
+            EXPECT_EQ(expectedQNames.at(i), b.FullName());
         }
     }
-
-    // temp file cleanup
-    ::remove(outBam.c_str());
-    ::remove(outPbi.c_str());
 }
+
+// clang-format on
