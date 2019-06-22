c987f2f6b804044627c9151b186aa7309abf6d9b
Derek Barnett <dbarnett@pacificbiosciences.com>
2019-06-21 21:36:31 -0500

    Initial CCSRecord data structures, serialization, and indexing

---
+++ include/pbbam/ccs/CCSRecordReader.h
@@ -0,0 +1,42 @@
+// Author: Derek Barnett
+
+#ifndef PBBAM_CCS_CCSRECORDREADER_H
+#define PBBAM_CCS_CCSRECORDREADER_H
+
+#include "pbbam/Config.h"
+
+#include <iosfwd>
+#include <memory>
+#include <string>
+
+#include "pbbam/ccs/CCSHeader.h"
+#include "pbbam/ccs/CCSRecord.h"
+#include "pbbam/internal/QueryBase.h"
+
+namespace PacBio {
+namespace CCS {
+
+///
+/// Reads CCSRecords from stdin
+//
+class CCSRecordReader : public PacBio::BAM::internal::QueryBase<CCSRecord>
+{
+public:
+    CCSRecordReader();
+    CCSRecordReader(std::istream& in);
+    ~CCSRecordReader();
+
+public:
+    const CCSHeader& Header() const;
+
+    bool GetNext(CCSRecord& record);
+
+private:
+    class CCSRecordReaderPrivate;
+    std::unique_ptr<CCSRecordReaderPrivate> d_;
+};
+
+}  // namespace CCS
+}  // namespace PacBio
+
+#endif  // PBBAM_CCS_CCSRECORD_H
