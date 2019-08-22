5b0afd03b931aeef0610e704dfc39bce492b7827
David Seifert <dseifert@pacificbiosciences.com>
2019-08-22 22:46:09 +0200

    Add Software 7.1 metadata

--- src/ChemistryTable.cpp
+++ src/ChemistryTable.cpp
@@ -137,14 +137,14 @@ const ChemistryTable& BuiltInChemistryTable()
         {{"101-717-400", "101-717-100", "5.0", "S/P3-C1/5.0-8M", "TAGT-418"}}, // Sequel® Dev Sequencing Plate Dyn Loading
 
         // Sequel® II Binding Kit 2.0; Sequel® II Sequencing Plate 2.0EA (4 Rxn)
-        {{"101-789-500", "101-789-300", "5.0", "S/P4-C2/5.0-8M"}},
+        {{"101-789-500", "101-789-300", "5.0", "S/P4-C2/5.0-8M", "TAGT-419"}},
         // Sequel® II Binding Kit 2.0; Sequel® II Sequencing Plate 2.0 (4 Rxn)
         {{"101-789-500", "101-826-100", "5.0", "S/P4-C2/5.0-8M", "TAGT-319"}},
         // Sequel® II Binding Kit 2.0; Sequel® II Sequencing Plate 2.0 (4 Rxn) - QC
         {{"101-789-500", "101-820-300", "5.0", "S/P4-C2/5.0-8M", "TAGT-319"}},
 
         // Sequel® II Binding Kit 2.1; Sequel® II Sequencing Plate 2.0EA (4 Rxn)
-        {{"101-820-500", "101-789-300", "5.0", "S/P4.1-C2/5.0-8M"}},
+        {{"101-820-500", "101-789-300", "5.0", "S/P4.1-C2/5.0-8M", "TAGT-419"}},
         // Sequel® II Binding Kit 2.1; Sequel® II Sequencing Plate 2.0 (4 Rxn)
         {{"101-820-500", "101-826-100", "5.0", "S/P4.1-C2/5.0-8M", "TAGT-319"}},
         // Sequel® II Binding Kit 2.1; Sequel® II Sequencing Plate 2.0 (4 Rxn) - QC
