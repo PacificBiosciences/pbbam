0c14281a1025867a71695cb1814962dbc75207e2
Armin Töpfer <atoepfer@pacificbiosciences.com>
2021-01-07 14:34:27 +0100

    Added wrapper for BamReader file handle & thread pool, for coordinating destruction.

--- src/BamReader.cpp
+++ src/BamReader.cpp
@@ -27,6 +27,24 @@
 namespace PacBio {
 namespace BAM {
 
+struct SamFileHandle
+{
+    htsThreadPool ThreadPool = {NULL, 0};
+    samFile* File = nullptr;
+
+    ~SamFileHandle()
+    {
+        if (File) {
+            sam_close(File);
+            File = nullptr;
+        }
+        if (ThreadPool.pool) {
+            hts_tpool_destroy(ThreadPool.pool);
+            ThreadPool.pool = nullptr;
+        }
+    }
+};
+
 class BamReader::BamReaderPrivate
 {
 public:
@@ -39,8 +57,8 @@ public:
                 return "\n  file: " + filename_;
         };
 
-        htsFile_.reset(sam_open(filename_.c_str(), "rb"));
-        if (!htsFile_ || !htsFile_->fp.bgzf) {
+        handle_.File = sam_open(filename_.c_str(), "rb");
+        if (!handle_.File || !handle_.File->fp.bgzf) {
             std::ostringstream s;
             s << "[pbbam] BAM reader ERROR: could not open for reading:" << displayFilename();
             throw std::runtime_error{s.str()};
@@ -57,8 +75,8 @@ public:
                       << envCStr;
                     throw std::runtime_error{s.str()};
                 }
-                p.pool = hts_tpool_init(numThreads);
-                hts_set_opt(htsFile_.get(), HTS_OPT_THREAD_POOL, &p);
+                handle_.ThreadPool.pool = hts_tpool_init(numThreads);
+                hts_set_opt(handle_.File, HTS_OPT_THREAD_POOL, &handle_.ThreadPool);
             } catch (const std::exception&) {
                 std::ostringstream s;
                 s << "[pbbam] BAM reader ERROR: environment variable PB_BAMREADER_THREADS is not a "
@@ -68,14 +86,14 @@ public:
             }
         }
 
-        const auto bgzfPos = bgzf_tell(htsFile_->fp.bgzf);
+        const auto bgzfPos = bgzf_tell(handle_.File->fp.bgzf);
         if (bgzfPos != 0) {
             std::ostringstream s;
             s << "[pbbam] BAM reader ERROR: could not read from empty input:" << displayFilename();
             throw std::runtime_error{s.str()};
         }
 
-        const std::unique_ptr<bam_hdr_t, HtslibHeaderDeleter> hdr(sam_hdr_read(htsFile_.get()));
+        const std::unique_ptr<bam_hdr_t, HtslibHeaderDeleter> hdr(sam_hdr_read(handle_.File));
         if (!hdr) {
             std::ostringstream s;
             s << "[pbbam] BAM reader ERROR: could not read header from:" << displayFilename();
@@ -85,14 +103,8 @@ public:
         header_ = BamHeaderMemory::FromRawData(hdr.get());
     }
 
-    ~BamReaderPrivate()
-    {
-        if (p.pool) hts_tpool_destroy(p.pool);
-    }
-
-    htsThreadPool p = {NULL, 0};
     std::string filename_;
-    std::unique_ptr<samFile, HtslibFileDeleter> htsFile_;
+    SamFileHandle handle_;
     BamHeader header_;
 };
 
@@ -107,7 +119,7 @@ BamReader::BamReader(BamFile bamFile) : BamReader{bamFile.Filename()} {}
 
 BamReader::~BamReader() = default;
 
-BGZF* BamReader::Bgzf() const { return d_->htsFile_->fp.bgzf; }
+BGZF* BamReader::Bgzf() const { return d_->handle_.File->fp.bgzf; }
 
 const std::string& BamReader::Filename() const { return d_->filename_; }
 
@@ -117,7 +129,7 @@ bool BamReader::GetNext(BamRecord& record)
 {
     assert(BamRecordMemory::GetRawData(record).get());
 
-    const auto result = ReadRawData(d_->htsFile_.get(), BamRecordMemory::GetRawData(record).get());
+    const auto result = ReadRawData(d_->handle_.File, BamRecordMemory::GetRawData(record).get());
 
     // success
     if (result >= 0) {
